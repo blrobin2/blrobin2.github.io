61475f9f63338e221a13a4484b7bc4ddb37dcc6a
Bennie <brobinson88@gmail.com>
2018-11-13 08:25:37 -0500

    initialize code styling

--- js/index.js
+++ js/index.js
@@ -5,8 +5,10 @@ WebFontConfig = {
 };
 
 (function(d) {
-    var wf = d.createElement('script'), s = d.scripts[0];
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-    wf.async = true;
-    s.parentNode.insertBefore(wf, s);
+  var wf = d.createElement('script'), s = d.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+  wf.async = true;
+  s.parentNode.insertBefore(wf, s);
+
+  hljs.initHighlightingOnLoad();
 })(document);
